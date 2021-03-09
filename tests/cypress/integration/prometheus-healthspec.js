131f900d0bd360a6cd8f5bd030ddc249eea1dc17
garcia.ryan <garcia.ryan@solute.us>
2021-03-09 11:34:48 -0700

    Updating prometheus cypress testing

--- tests/cypress/integration/prometheus-healthspec.js
+++ tests/cypress/integration/prometheus-healthspec.js
@@ -9,5 +9,24 @@ describe('Basic prometheus', function() {
       cy.get('input[name="submit"]')
         .click({waitForAnimations: false})
     })
-     
-})  
+
+    it('Validate prometheus targets', function() {
+      // Make sure we have expected targets.  Come targets may have variable number of instances thus we
+      // have to allow for a variable number of running instances.
+      cy.wait(90000)
+      cy.visit(`https://prometheus.bigbang.dev/targets`)
+      cy.contains(/monitoring\/alertmanager\/0 +\(\d+\/\d+ +up\)/)
+      cy.contains(/monitoring\/coredns\/0 +\(\d+\/\d+ +up\)/)
+      cy.contains(/monitoring\/grafana\/0 +\(\d+\/\d+ +up\)/)
+      cy.contains(/monitoring\/kube-apiserver\/0 +\(\d+\/\d+ +up\)/)
+      cy.contains(/monitoring\/istio-envoy\/0 +\(\d+\/\d+ +up\)/)
+      cy.contains(/monitoring\/istio-pilot\/0 +\(\d+\/\d+ +up\)/)
+      cy.contains(/monitoring\/kube-state-metrics\/0 +\(\d+\/\d+ +up\)/)
+      cy.contains(/monitoring\/kubelet\/0 +\(\d+\/\d+ +up\)/)
+      cy.contains(/monitoring\/kubelet\/1 +\(\d+\/\d+ +up\)/)
+      cy.contains(/monitoring\/kubelet\/2 +\(\d+\/\d+ +up\)/)
+      cy.contains(/monitoring\/node-exporter\/0 +\(\d+\/\d+ +up\)/)
+      cy.contains(/monitoring\/prometheus-operator\/0 +\(\d+\/\d+ +up\)/)
+      cy.contains(/monitoring\/prometheus\/0 +\(\d+\/\d+ +up\)/)
+    })
+})
