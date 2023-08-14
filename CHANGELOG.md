3c5d4b535c2012677c3a2a4e7d0edd593a190b42
Jeffrey Victor <jeffrey.victor.1.ctr@us.af.mil>
2023-08-14 13:37:29 +0000

    Keycloak Monitoring sso cypress

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,11 @@
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 ---
+
+## [47.1.0-bb.3] - 2023-08-14
+### Changed
+- Updated Cypress tests to allow for SSO testing
+
 ## [47.1.0-bb.2] - 2023-07-31
 ### Added
 - cap drop ALL to clean upgrade
--- chart/tests/cypress/alertmanager-healthspec.js
+++ chart/tests/cypress/alertmanager-healthspec.js
@@ -1,8 +1,23 @@
-import './login.js'
-
 before (function() {
   cy.visit(Cypress.env('alertmanager_url'))
-  cy.login()
+  if (Cypress.env('keycloak_test_enable')) {
+    cy.task('log', 'logging in via keycloak...')
+          cy.get('input[id="username"]')
+            .type(Cypress.env('tnr_username'))
+            .should('have.value', Cypress.env('tnr_username'));
+
+          cy.get('input[id="password"]')
+            .type(Cypress.env('tnr_password'))
+            .should('have.value', Cypress.env('tnr_password'));
+            
+          cy.get('form').submit(); 
+
+          cy.get('input[id="kc-accept"]').click(); 
+
+          cy.get('input[id="kc-login"]').click(); 
+          }
+//  cy.login()
+  cy.wait(200)
 })
 
 describe('Alertmanager unit testing', function() {
